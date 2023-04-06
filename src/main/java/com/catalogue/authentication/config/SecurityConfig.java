e83634c079587eb1c6f30c16e5262ead0a1be389
Okayreet <darkerror@hotmail.co.uk>
2023-04-06 19:31:07 +0100

    update

--- src/main/java/com/catalogue/authentication/config/SecurityConfig.java
+++ src/main/java/com/catalogue/authentication/config/SecurityConfig.java
@@ -2,46 +2,64 @@ package com.catalogue.authentication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
+// @EnableWebSecurity
+// @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-private final UserDetailsServiceImpl userDetailsServiceImpl;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/products/welcome","/products/new").permitAll()
-                .and()
-                .authorizeHttpRequests().requestMatchers("/products/**")
-                .authenticated().and().formLogin().and().build();
-    }
+    // @Bean
+    // SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+    //     return http
+    //             .authorizeHttpRequests(auth -> {
+    //                 auth.requestMatchers("/api/v1/login", "/api/v1/authenticate").permitAll();
+    //                 // auth.requestMatchers("/api/posts/**").hasRole("ADMIN");
+    //                 auth.anyRequest().authenticated();
+    //             })
+    //             .csrf(AbstractHttpConfigurer::disable)
+    //             .sessionManagement(management -> management.sessionCreationPolicy(STATELESS))
+    //             .httpBasic(Customizer.withDefaults())
+    //             .build();
+    // }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsServiceImpl);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
+    // @Bean
+    // AuthenticationProvider authenticationProvider() {
+    //     DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    //     authenticationProvider.setUserDetailsService(userDetailsServiceImpl);
+    //     authenticationProvider.setPasswordEncoder(passwordEncoder());
+    //     return authenticationProvider;
+    // }
 
+    @Bean
+    AuthenticationManager authenticationManagerBean(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(
+                userDetailsServiceImpl)
+                .passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
+    }
 }
