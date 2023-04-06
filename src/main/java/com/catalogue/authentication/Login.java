beb4faab4f960ee45d1322dc8eba9fda20a15e06
Okayreet <darkerror@hotmail.co.uk>
2023-04-06 17:06:35 +0100

    AUTHENTICATION SERVICE

---
+++ src/main/java/com/catalogue/authentication/Login.java
@@ -0,0 +1,46 @@
+package com.catalogue.authentication;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "email_unique", columnNames = "email")
+})
+public class Login {
+    @Id
+    @SequenceGenerator(name = "login_sequence", sequenceName = "login_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "login_sequence")
+    @JsonIgnore
+    private Long id;
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Must include @,. and top level domain (.com, .uk etc) ",
+            regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-]" +
+                    "[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
+    private String email;
+    @NotBlank(message = "Password cannot be blank")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    @NotBlank(message = "Role cannot be blank")
+    private String role;
+
+    @JsonIgnore
+    public List<String> getRole() {
+        return Collections.singletonList(role);
+    }
+    private Long userId;
+
+}
